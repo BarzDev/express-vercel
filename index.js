73aaca5277c2022a9ddbdaa3928c5818452b2f0d
Fakhrul <barz.develop@gmail.com>
2024-05-10 22:08:41 +0700

    add: test json response

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.get("/tes", (req, res) => {
   res.send("hello app");
 });
 
+app.get("/json", (req, res) => {
+  const data = {
+    message: "Hello from Express on Vercel",
+    status: "success",
+  };
+
+  res.json(data);
+});
+
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
