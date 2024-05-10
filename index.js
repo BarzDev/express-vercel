0a93318a573937f6d6461d9e70ec2dc3f3b2439f
Fakhrul <barz.develop@gmail.com>
2024-05-10 22:02:58 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Add Express
+const express = require("express");
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+app.get("/tes", (req, res) => {
+  res.send("hello app");
+});
+
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+// Export the Express API
+module.exports = app;
