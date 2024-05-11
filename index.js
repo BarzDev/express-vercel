281aabbeec5390672cdd2ed6d4402b60609aae25
Fakhrul <barz.develop@gmail.com>
2024-05-11 23:34:33 +0700

    add: middleware

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 // Add Express
 const express = require("express");
+const { apiKeyAuth } = require("@vpriem/express-api-key-auth");
+const dotenv = require("dotenv");
 
 // Initialize Express
 const app = express();
+
+// Import .env
+dotenv.config();
+const apiKey1 = process.env.API_KEY_1;
+const apiKey2 = process.env.API_KEY_2;
+console.log({ apiKey1, apiKey2 });
+
+// Add midleware auth
+const apiKeyMiddleware = apiKeyAuth([apiKey1, apiKey2]);
+app.use(apiKeyMiddleware);
 
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 app.get("/tes", (req, res) => {
+  console.log("tes router");
   res.send("hello app");
 });
 
